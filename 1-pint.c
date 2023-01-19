75b68f26f05fc862eef38967c572adb7a13af12d
Prisoner218070144 <Qrikhotso@yahoo.com>
2023-01-19 13:54:39 +0200

    having the pint function only to test

--- 1-pint.c
+++ 1-pint.c
@@ -1,37 +1,3 @@
-#include "monty.h"
-
-/**
-  * create_stack - Creating the stack
-  * @size: The size of the stack
-  *
-  * Return: stack
-  */
-stack_t *create_stack(int size)
-{
-    stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
-    stack->stack = (int *)malloc(sizeof(int) * size);
-    stack->top = -1;
-    stack->size = size;
-    return stack;
-}
-
-/**
-  * push - Pushes an element to the stack
-  * @stack: Stack
-  * @value: Content to be added to stack
-  *
-  * Return: Nothing
-  */
-void push(stack_t *stack, int value)
-{
-    if (stack->top == stack->size - 1) {
-        printf("Error: stack overflow\n");
-        return;
-    }
-    stack->top++;
-    stack->stack[stack->top] = value;
-}
-
 /**
   * pint - Prints the value at the top of the stack
   * @stack: Stack
@@ -45,34 +11,4 @@ void pint(stack_t *stack)
         return;
     }
     printf("%d\n", stack->stack[stack->top]);
-}
-
-/**
-  * pall - Prints all the values on the stack
-  * @stack: Stack
-  *
-  * Return: Nothing
-  */
-void pall(stack_t *stack)
-{
-    for (int i = 0; i <= stack->top; i++) {
-        printf("%d\n", stack->stack[i]);
-    }
-}
-
-/**
-  * main - Entry point
-  *
-  * Return: 0 always
-  */
-int main()
-{
-    stack_t *stack = create_stack(10);
-    push(stack, 1);
-	pint(stack);
-    push(stack, 2);
-	pint(stack);
-    push(stack, 3);
-    pint(stack);
-    return 0;
 }
