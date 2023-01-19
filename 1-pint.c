e748cab27e7aa019780a5b360781d59ef3f8e55f
Prisoner218070144 <Qrikhotso@yahoo.com>
2023-01-19 13:40:59 +0200

    Added a main function

--- 1-pint.c
+++ 1-pint.c
@@ -59,3 +59,20 @@ void pall(stack_t *stack)
         printf("%d\n", stack->stack[i]);
     }
 }
+
+/**
+  * main - Entry point
+  *
+  * Return: 0 always
+  */
+int main()
+{
+    stack_t *stack = create_stack(10);
+    push(stack, 1);
+	pint(stack);
+    push(stack, 2);
+	pint(stack);
+    push(stack, 3);
+    pint(stack);
+    return 0;
+}
