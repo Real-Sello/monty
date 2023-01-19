067d261c14596d754ded0824e1f9deafb483d0bd
Prisoner218070144 <Qrikhotso@yahoo.com>
2023-01-19 14:18:28 +0200

    Added all the opcode in one file

--- 0-push_pall.c
+++ 0-push_pall.c
@@ -48,7 +48,8 @@ int _atoi(char *s)
   */
 void push(stack_t *stack, int value)
 {
-    if (stack->top == stack->size - 1) {
+    if (stack->top == stack->size - 1)
+	{
         printf("Error: stack overflow\n");
         return;
     }
@@ -64,9 +65,62 @@ void push(stack_t *stack, int value)
   */
 void pall(stack_t *stack)
 {
-    for (int i = 0; i <= stack->top; i++) {
+    for (int i = 0; i <= stack->top; i++)
+	{
         printf("%d\n", stack->stack[i]);
     }
+}
+
+int pop(stack_t *stack)
+{
+    if (stack->top == -1)
+	{
+        printf("Error: stack underflow\n");
+        return -1;
+    }
+    int value = stack->stack[stack->top];
+    stack->top--;
+    return value;
+}
+
+/**
+  * pint - Prints the value at the top of the stack
+  * @stack: Stack
+  *
+  * Return: Nothing
+  */
+void pint(stack_t *stack)
+{
+    if (stack->top == -1)
+	{
+        printf("Error: stack underflow\n");
+        return;
+    }
+    printf("%d\n", stack->stack[stack->top]);
+}
+
+void swap(stack_t *stack)
+{
+    if (stack->top < 1)
+	{
+        printf("Error: not enough elements in stack to swap\n");
+        return;
+    }
+    int temp = stack->stack[stack->top];
+    stack->stack[stack->top] = stack->stack[stack->top - 1];
+    stack->stack[stack->top - 1] = temp;
+}
+
+void add(stack_t *stack)
+{
+    if (stack->top < 1)
+	{
+        printf("Error: not enough elements in stack to add\n");
+        return;
+    }
+    int a = pop(stack);
+    int b = pop(stack);
+    push(stack, a + b);
 }
 
 /**
