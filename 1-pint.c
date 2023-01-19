f8e68065bfcf69311f7730ec96fb96f5db153ac9
Prisoner218070144 <Qrikhotso@yahoo.com>
2023-01-19 13:58:07 +0200

    stack

--- 1-pint.c
+++ 1-pint.c
@@ -1,3 +1,20 @@
+#include "monty.h"
+
+/**
+  * create_stack - Creating the stack
+  * @size: The size of the stack
+  *
+  * Return: stack
+  */
+stack_t *create_stack(int size)
+{
+    stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
+    stack->stack = (int *)malloc(sizeof(int) * size);
+    stack->top = -1;
+    stack->size = size;
+    return stack;
+}
+
 /**
   * pint - Prints the value at the top of the stack
   * @stack: Stack
