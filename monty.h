42d7fd81c70d96bbe1b7f656bafca90f60a2038d
Prisoner218070144 <Qrikhotso@yahoo.com>
2023-01-19 14:30:30 +0200

    Added the data structures we have to use

--- monty.h
+++ monty.h
@@ -5,40 +5,36 @@
 #include <stdlib.h>
 
 /**
- * struct stack_s - Stack
- * @stack: .....
- * @top: ......
- * @size: .......
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
- * Description: stack.......
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
-typedef struct stack_s {
-    int *stack;
-    int top;
-    int size;
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
 
 /**
-  * create_stack - Creating the stack
-  * @size: The size of the stack
-  *
-  * Return: stack
-  */
-stack_t *create_stack(int size)
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
 {
-    stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
-    stack->stack = (int *)malloc(sizeof(int) * size);
-    stack->top = -1;
-    stack->size = size;
-    return stack;
-}
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
-int _atoi(char *s);
-void push(stack_t *stack, int value);
-void pall(stack_t *stack);
-void pint(stack_t *stack);
-void swap(stack_t *stack);
-void pint(stack_t *stack);
-void add(stack_t *stack);
+
+
 
 #endif
